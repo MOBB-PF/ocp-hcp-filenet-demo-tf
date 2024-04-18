81e74ce0f66d747f3701ee209089a18c2a5b7fb7
Drew Anderson <dranders@redhat.com>
2024-04-18 16:53:32 +1000

    add efs policy

---
+++ terraform/efs.tf
@@ -0,0 +1,48 @@
+# https://cloud.redhat.com/experts/rosa/aws-efs/
+
+resource "aws_iam_policy" "rosa-efs-csi-policy" {
+  name        = "${var.cluster_name}-rosa-efs-csi"
+  path        = "/"
+  description = "AWS EFS CSI Driver Policy"
+
+  policy = jsonencode({
+    Version = "2012-10-17"
+    Statement = [
+      {
+        Effect = "Allow"
+        Action = [
+          "elasticfilesystem:DescribeAccessPoints",
+          "elasticfilesystem:DescribeFileSystems",
+          "elasticfilesystem:DescribeMountTargets",
+          "elasticfilesystem:TagResource",
+          "ec2:DescribeAvailabilityZones"
+        ]
+        Resource = "*"
+      },
+      {
+        Effect = "Allow"
+        Action = [
+          "elasticfilesystem:CreateAccessPoint"
+        ]
+        Resource = "*"
+        Condition = {
+          StringLike = {
+            "aws:RequestTag/efs.csi.aws.com/cluster" = "true"
+          }
+        }
+      },
+      {
+        Effect = "Allow"
+        Action = [
+          "elasticfilesystem:DeleteAccessPoint",
+        ]
+        Resource = "*"
+        Condition = {
+          StringEquals = {
+            "aws:ResourceTag/efs.csi.aws.com/cluster" = "true"
+          }
+        }
+      }
+    ]
+  })
+}
